c09e53f9c39655ce6f7866f64b190f1b68cdbba7
gmentzik <65217733+gmentzik@users.noreply.github.com>
2023-02-19 09:41:32 +0200

    Update validateJson.js
    Keep only a couple of calling validation examples

--- validateJson.js
+++ validateJson.js
@@ -77,35 +77,15 @@ const validateArrayOfObjs = (stateObj, jsonArrayKey, requiredFieldsArr,maxAllowe
 let csv2jsonState = $("State.csvtojson_scenario_state") != null ? $("State.csvtojson_scenario_state") : {};
 let jsonKey = '';
 let required = [];
-//global
-jsonKey = 'global';
-required = ['scenario_id','raw_cm_retention','raw_pm_retention','raw_trace_retention','require_hw_inspection','sp_extras_scenarios',
-'sp_extras_hw_reuse','sp_extras_expansion','sp_extras_multi_tenant','sp_extras_multi_vendor','purchase_new_servers','infrastructure_required',
-'project_years','sp_extras_security','project_type'];
-csv2jsonState = validateArrayOfObjs(csv2jsonState, jsonKey, required,1);
-// footprint
-jsonKey = 'footprint';
-required = ['scenario_id','flavor_code','sw_module_id'];
-csv2jsonState = validateArrayOfObjs(csv2jsonState, jsonKey, required);
+
+// CALL VALIDATION FUNCTION EXAMPLES (updates Budibase state)
 // pmi
 jsonKey = 'pmi';
 required = ['module_input_value','scenario_id','setting_id'];
 csv2jsonState = validateArrayOfObjs(csv2jsonState, jsonKey, required);
-// bom
-jsonKey = 'bom';
-required = ['scenario_id','material_id','elements'];
-csv2jsonState = validateArrayOfObjs(csv2jsonState, jsonKey, required);
 // vm
 jsonKey = 'vm';
 required = ['scenario_id','node_type_id','servers','vcpus','ram','storage'];
-csv2jsonState = validateArrayOfObjs(csv2jsonState, jsonKey, required);
-// cost
-jsonKey = 'cost';
-required = ['scenario_id','cost_year','cost_item_id','cost_amount'];
-csv2jsonState = validateArrayOfObjs(csv2jsonState, jsonKey, required);
-// tenant
-jsonKey = 'tenant';
-required = ['scenario_id','opco_id','flavor_code','sw_ran_release','cells_count','nodes_count','oss_instances','bsc_rnc_count'];
 csv2jsonState = validateArrayOfObjs(csv2jsonState, jsonKey, required);
 
 return csv2jsonState;
